3b8123f536a47b5576ffc617b56dd945353a3729
MushfiqueAlam <mushfique.037@gmail.com>
2025-01-22 01:20:05 +0530

    overloading example

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,24 +3,22 @@ package com.driver;
 public class Main {
     static class Product{
         public int product(int x, int y) {
-            return x+y;
+            return x*y;
         }
         public int product(int x, int y, int z) {
-            return x+y+z;
+            return x*y*z;
         }
         public double product(double x, double y) {
-            return x+y;
+            return x*y;
         }
 
     }
 
     public static void main(String[] args) {
         Product p=new Product();
-//        System.out.println(p.product(37,37));
-//        System.out.println(p.product(2,4,5));
-//        System.out.println(p.product(3.5,6.7));
-        p.product(2,3);
-        p.product(2,3,4);
-        p.product(2.5,6.5);
+        System.out.println(p.product(37,37));
+        System.out.println(p.product(2,4,5));
+        System.out.println(p.product(3.5,6.7));
+
     }
 }
